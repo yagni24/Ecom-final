1bf1559caacaa2bf81ce9ad9aff28262d90b8957
yagni24 <121762864+yagni24@users.noreply.github.com>
2023-05-26 13:37:41 +0530

    Delete theme.js

--- theme.js
+++
@@ -1,9 +0,0 @@
-import {createTheme } from "@mui/material/styles";
-const theme =  createTheme({
-    palette:{
-        primary:{
-            main:'#81fcdb',
-        },
-    },
-});
-export default theme;
